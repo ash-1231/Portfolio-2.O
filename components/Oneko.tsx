a3cb52a13aa95530a0916aa635a6e98675a9a8e5
ashutosh <ashutoshpro11@gmail.com>
2025-09-16 17:09:23 +0530

    Update Oneko.tsx

--- components/Oneko.tsx
+++ components/Oneko.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useEffect } from "react"
 
-export default function (){
+export default function Oneko(){
     useEffect(() => {
         const script = document.createElement("script");
         script.src = "/oneko.js"; // path in /public
